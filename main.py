5184f0e1cba8bd18bcd74cb4c6403e32b0ff1e99
ChernyshevVyacheslav <81693471+ChernyshevVyacheslav@users.noreply.github.com>
2021-04-28 21:57:52 +0300

    Registered users adds to MongoDb (#2)
    * Registered users adds to MongoDb
    
    * New users adds to mongo datbase
    
    * New users adds to mongo datbase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+from pymongo import MongoClient
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,9 +19,12 @@ def register():
         password_retry = request.form.get('password-retry')
         
         if password == password_retry:
+            db.users.insert_one(
+                {"username": username, "email": email, "fname": fname, "lname": lname, "password": password})
             return redirect(url_for('profile', username=username))
         else:
             return redirect(url_for('register'))
+
 
 @app.route('/profile/', methods = ['GET'])
 def profile():
@@ -29,4 +32,6 @@ def profile():
     return render_template('profile.html', username=username)
 
 if __name__ == "__main__":
+    client = MongoClient('localhost', 27017)
+    db = client.meet_db
     app.run(host='localhost', port=5000, debug=True)
